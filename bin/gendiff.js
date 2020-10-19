16b91b907f04f72237c5478347313e3a79b9e357
alex-kurkov <alexkourkov@yandex.ru>
2020-10-19 21:35:39 +0500

    refactor gendiff args handler

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import commander from 'commander';
+import fs from 'fs';
 import path from 'path';
 const gendiff = commander.program;
 
@@ -9,18 +10,19 @@ gendiff
   .name('gendiff')
   .usage('[options]')
   .helpOption('-h, --help', 'output usage information')
- 
-gendiff
   .option('-f, --format [type]', 'output format', 'json')
+  .arguments('<path1> <path2>')
 
-gendiff
-  .arguments('<pathToFile1> <pathToFile1>')
-
-const file1 = path.resolve('${gendiff.pathToFile1}')
-console.log(file1);
-
-// eslint-disable-next-line no-undef
+  // eslint-disable-next-line no-undef
 gendiff.parse(process.argv);
+  // eslint-disable-next-line no-undef
+const __workingDirectory = process.cwd();
+const pathToFile1 = path.resolve(__workingDirectory, gendiff.args.shift());
+const pathToFile2 = path.resolve(__workingDirectory, gendiff.args.shift());
 
+
+console.log(pathToFile2)
+
+console.log(gendiff.args);
 console.log('Options: ', gendiff.opts());
 console.log('Remaining arguments: ', gendiff.args);

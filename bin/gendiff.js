84a7a2e9487b2a493df9006d8ac202c20a94098f
alex-kurkov <alexkourkov@yandex.ru>
2020-10-19 20:16:33 +0500

    add args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 import commander from 'commander';
-
+import path from 'path';
 const gendiff = commander.program;
 
 gendiff
-  .version('0.0.1', '-V, --version', 'output the version number');
-gendiff
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
   .name('gendiff')
-  .usage('[options]');
+  .usage('[options]')
+  .helpOption('-h, --help', 'output usage information')
+ 
 gendiff
-  .helpOption('-h --help', 'output usage information');
+  .option('-f, --format [type]', 'output format', 'json')
+
+gendiff
+  .arguments('<pathToFile1> <pathToFile1>')
+
+const file1 = path.resolve('${gendiff.pathToFile1}')
+console.log(file1);
 
 // eslint-disable-next-line no-undef
 gendiff.parse(process.argv);
+
+console.log('Options: ', gendiff.opts());
+console.log('Remaining arguments: ', gendiff.args);

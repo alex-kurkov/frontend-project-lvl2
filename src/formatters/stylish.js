7eb65b8ae5d5823afd04a2d206a9f67d30502e46
alex-kurkov <alexkourkov@yandex.ru>
2020-10-23 15:13:56 +0500

    add array render

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,18 +4,33 @@ export default (value, renderPrefix = '  ') => {
       return currentValue.toString();
     }
     if (currentValue === null) return 'null';
-    
+
     const deepIndentSize = depth + 1;
     const deepIndent = renderPrefix.repeat(deepIndentSize);
     const currentIndent = renderPrefix.repeat(depth) + renderPrefix.repeat(depth);
+    
+    if (Array.isArray(currentValue)) {
+      const lines = currentValue
+        .flatMap((val) => {
+          if (typeof val !== 'object' || value === null) {
+            return `${deepIndent.repeat(2)}${val}`
+          }
+          return `${deepIndent.repeat(2)}${iter(val, deepIndentSize)}`;
+        })
+      return [
+        '[',
+        ...lines,
+        `${currentIndent}]`,
+      ].join('\n');
+    }
 
     const lines = Object
     .entries(currentValue)
     .map(([key, val]) => {
       const keyArr = key.split(' ');
       const unprefixedKey = keyArr.pop();
-      const prefixIndicator = keyArr.shift(); // may be '+','-' or ''
-      const keyPrefixer = (prefixIndicator !== '') 
+      const prefixIndicator = keyArr.shift(); // may be '+','-' or undefined
+      const keyPrefixer = (prefixIndicator !== undefined)
         ? `${deepIndent.slice(0, -2)}${prefixIndicator} `
         : deepIndent;
 
--- src/index.js
+++ src/index.js
@@ -8,27 +8,13 @@ const uniq = (arr1, arr2) => arr2
   }, [...arr1])
   .sort();
 
-const objectKeyDeepPrefixer = (value) => {
-  if (typeof value !== 'object') return value;
-  if (value === null) return null;
-  return Object
-    .entries(value)
-    .reduce((acc, [key, value]) => {
-      const newKey = `  ${key}`;
-      acc[newKey] = objectKeyDeepPrefixer(value);
-      return acc;
-    }, {})
-}
-
-
 const comparer = (object1, object2) => {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
   const uniqueKeys = uniq(keys1, keys2);
 
   const compared = uniqueKeys.reduce((acc, key) => {
-    
-    const bothHaveKey = `  ${key}`;
+    const bothHaveKey = key;
     const firstOnlyKey = `- ${key}`;
     const secondOnlyKey = `+ ${key}`;
     const value1 = object1[key];
@@ -39,18 +25,17 @@ const comparer = (object1, object2) => {
       return acc;
     }
     if (typeof value1 === 'object' && typeof value2 === 'object') {
-      acc[bothHaveKey] = comparer(value1, value2);
-      return acc;
+        acc[bothHaveKey] = comparer(value1, value2);
+        return acc;
     }
     if (Object.prototype.hasOwnProperty.call(object1, key)) {
-      acc[firstOnlyKey] = objectKeyDeepPrefixer(value1); // NB!!! needs prefixer
+      acc[firstOnlyKey] = value1;
     }
     if (Object.prototype.hasOwnProperty.call(object2, key)) {
-      acc[secondOnlyKey] = objectKeyDeepPrefixer(value2); // NB!!! needs prefixer
+      acc[secondOnlyKey] = value2;
     }
     return acc;
     }, {});
-
   return compared;
 };
 

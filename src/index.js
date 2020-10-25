36b893e892e1b0d6e4d6b86ee23bb7c2a566558f
alex-kurkov <alexkourkov@yandex.ru>
2020-10-25 12:33:49 +0500

    recomposed index.js

--- src/index.js
+++ src/index.js
@@ -17,20 +17,51 @@ const setImmutableChildrenNodes = (object) => {
 };
 
 // returns sorted unique keys
-const uniq = (arr1, arr2) => arr2
-  .reduce((acc, i) => {
-    return (acc.includes(i)) ? acc : [...acc, i];
-  }, [...arr1])
-  .sort();
- 
+const getUniqueKeys = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  return keys2
+    .reduce((acc, i) => {
+      return (acc.includes(i)) ? acc : [...acc, i];
+    }, [...keys1])
+    .sort();
+}
 // simplified linter-friendly notation
 const hasKey = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+// mutated node add handler
+const handleNodeAdd = (object1, object2, key, value1, value2, node, acc) => {
+  if (hasKey(object1, key)) {
+    node['type'] = 'removed';
+    if (hasKey(object2, key)) {
+      node['updated'] = true;
+    }
+    if (value1 !== null && typeof value1 === 'object') {
+        node[`children`] = setImmutableChildrenNodes(value1);
+    } else {
+        node['value'] = value1;
+    }
+    acc.push(node);
+  }
+  if (hasKey(object2, key)) {
+    const node2 = { name: key, type: 'added'};
+
+    if (hasKey(object1, key)) {
+      node2['updated'] = true;
+      node2['updatedFrom'] = value1;
+    }
+
+  if (value2 !== null && typeof value2 === 'object') {
+        node2[`children`] = setImmutableChildrenNodes(value2);
+    } else {
+        node2['value'] = value2;
+    }
+  acc.push(node2);
+  }
+}
 
 // main comparer function returning tree-layout deep difference between 2 objects 
 const getDiff = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  const uniqueKeys = uniq(keys1, keys2);
+  const uniqueKeys = getUniqueKeys(object1, object2);
 
   const diff = uniqueKeys.reduce((acc, key) => {
     const node = { name: key, type: 'immuted' }; // default 'state'
@@ -41,34 +72,9 @@ const getDiff = (object1, object2) => {
     if (typeof value1 === 'object' && typeof value2 === 'object' ) {
       return [...acc, {...node, children: getDiff(value1, value2)}];
     }
-    // mutated node adders
-    if (hasKey(object1, key)) {
-      node['type'] = 'removed';
-      if (hasKey(object2, key)) {
-        node['updated'] = true;
-      }
-      if (value1 !== null && typeof value1 === 'object') {
-          node[`children`] = setImmutableChildrenNodes(value1);
-      } else {
-          node['value'] = value1;
-      }
-      acc.push(node);
-    }
-    if (hasKey(object2, key)) {
-      const node2 = { name: key, type: 'added'};
-
-      if (hasKey(object1, key)) {
-        node2['updated'] = true;
-        node2['updatedFrom'] = value1;
-      }
-
-    if (value2 !== null && typeof value2 === 'object') {
-          node2[`children`] = setImmutableChildrenNodes(value2);
-      } else {
-          node2['value'] = value2;
-      }
-    acc.push(node2);
-    }
+    // mutated node adder
+    handleNodeAdd(object1, object2, key, value1, value2, node, acc);
+  
     return acc;
     }, []);
   return diff;
@@ -79,4 +85,4 @@ export default (path1, path2, formatOutput = 'stylish') => {
   const obj2 = fileParser(path2);
   const result = getDiff(obj1, obj2);
   return formatter(result, formatOutput);
-}
+};

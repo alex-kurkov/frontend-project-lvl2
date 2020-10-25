4d661d55d042dbe0861eedf62f6abbed7a2c952b
alex-kurkov <alexkourkov@yandex.ru>
2020-10-25 14:23:00 +0500

    lint babel config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
-/*eslint no-undef: "error"*/
-/*eslint-env node*/
+/* eslint no-undef: "error" */
+/* eslint-env node */
 
 const presets = [
   ['@babel/preset-env', {
     targets: {
-      node: 'current'
-    }}]]
+      node: 'current',
+    },
+  }]];
 
 module.exports = { presets };

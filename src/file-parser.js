63c34c9c21ab913d6e98ac5cb3ce0c44b1ab9a51
alex-kurkov <alexkourkov@yandex.ru>
2020-10-29 22:52:08 +0500

    fix parser

--- src/file-parser.js
+++ src/file-parser.js
@@ -8,6 +8,12 @@ export default (filePath) => {
   try {
     return JSON.parse(data);
   } catch {
-    return yaml.safeLoad(data);
+    console.log('json parsing faild, trying another parser');
   }
+  try {
+    return yaml.safeLoad(data);
+  } catch {
+    console.log('yaml parsing faild');
+  }
+  return new Error('cannot read file');
 };

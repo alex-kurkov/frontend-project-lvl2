e42c23ed2ad3e45530ebd1b14adb62e5ed593489
alex-kurkov <alexkourkov@yandex.ru>
2020-10-29 21:21:32 +0500

    fix parser

--- src/file-parser.js
+++ src/file-parser.js
@@ -1,20 +1,13 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
-import path from 'path';
 
 export default (filePath) => {
   if (!filePath) return null;
-  const extension = path.extname(filePath);
   const data = fs.readFileSync(filePath, 'utf-8');
 
-  switch (extension) {
-    case '.yaml':
-      return yaml.safeLoad(data);
-    case '.yml':
-      return yaml.safeLoad(data);
-    case '.json':
-      return JSON.parse(data);
-    default:
-      return new Error('file not supported');
+  try {
+    return JSON.parse(data);
+  } catch {
+    return yaml.safeLoad(data);
   }
 };

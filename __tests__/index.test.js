55e194f7b5dbe07e63a4e3d3b1341d193c3fbc8f
alex-kurkov <alexkourkov@yandex.ru>
2020-11-21 20:23:23 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect, describe } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
@@ -9,21 +9,23 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe.each([
-  ['before1.json', 'before2.json', 'after.json', 'json', /\+ timeout/],
-  ['before1.yaml', 'before2.yaml', 'after.json', 'json', /- follow/],
-  ['before-nested1.json', 'before-nested2.json', 'after-stylish', 'stylish', /\+ setting4: blah blah/],
-  ['before-nested1.json', 'before-nested2.json', 'after-plain', 'plain',
-    /Property 'common\.setting6\.doge\.wow' was updated\. From '' to 'so much'/],
-  ['before-nested1.json', 'before-nested2.json', 'after-nested.json', 'json',
-    /"- group2":{"abc":12345,"deep":{"id":45}}/],
-])('check %s compared to %s', (file1, file2, resultFile, output, pattern) => {
-  test(`results in ${resultFile} formatted in mode: ${output}`, async () => {
-    const path1 = await getFixturePath(file1);
-    const path2 = await getFixturePath(file2);
-    const result = await readFile(resultFile).trim();
-    expect(getDifference(path1, path2, output)).toEqual(result);
-    expect(getDifference(path1, path2, output)).not.toHaveLength(0);
-    expect(getDifference(path1, path2, output)).toMatch(pattern);
+const expectedFlatJson = readFile('result.json', 'utf-8');
+const expectedNestedJson = readFile('resultnested.json', 'utf-8');
+const expectedStylish = readFile('resultstylish', 'utf-8');
+const expectedPlain = readFile('resultplain', 'utf-8');
+
+test.each`
+file1                      |file2                   |expected              |mode
+${'file1.json'}            |${'file2.json'}         |${expectedFlatJson}   |${'json'}
+${'file1.yaml'}            |${'file2.yaml'}         |${expectedFlatJson}   |${'json'}
+${'nestedfile1.json'}      |${'nestedfile2.json'}   |${expectedStylish}    |${'stylish'}
+${'nestedfile1.json'}      |${'nestedfile2.json'}   |${expectedPlain}      |${'plain'}
+${'nestedfile1.json'}      |${'nestedfile2.json'}   |${expectedNestedJson} |${'json'}
+`('check $file1 compared to $file2 formatted in mode: $mode results in $expected',
+  ({
+    file1, file2, expected, mode,
+  }) => {
+    const path1 = getFixturePath(file1);
+    const path2 = getFixturePath(file2);
+    expect(getDifference(path1, path2, mode)).toEqual(expected);
   });
-});
